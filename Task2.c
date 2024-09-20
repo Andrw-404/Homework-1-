f2ec9e3b77add3b1b9f2a4a86d143d00851b4fcf
Andrw-404 <98185584+Andrw-404@users.noreply.github.com>
2024-09-20 14:05:17 +0300

    Update Task2.c

--- Task2.c
+++ Task2.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 void swap(int* left, int* right) {
-	*left ^= *right;
-	*right ^= *left;
-	*left ^= *right;
+	if (left != right) {
+		*left ^= *right;
+		*right ^= *left;
+		*left ^= *right;
+	}
 }
 
 void main() {
 	int a = 3;
-	int b = 12;
+	int b = 5;
 	swap(&a, &b);
 	printf("a = %d; b = %d\n", a, b);
 }
