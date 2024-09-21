db7029370175eb18f1ffa85a915200dc1cb0214a
Andrw-404 <98185584+Andrw-404@users.noreply.github.com>
2024-09-21 23:17:50 +0300

    Update Task3.c

--- Task3.c
+++ Task3.c
@@ -2,39 +2,47 @@
 #include <math.h>
 #include <locale.h>
 
-int main(void) {
-	setlocale(LC_ALL, "Rus");
-	int firstNumber = 0;
-	int secondNumber = 0;
-	int result = 0;
-	printf("Введите делимое и делитель:\n");
-	scanf_s("%d %d", &firstNumber, &secondNumber);
+
+int incompleteQuotient(int firstNumber, int secondNumber, int* errorCode) {
+	int counter = 0;
 	if (secondNumber == 0) {
-		printf("На ноль делить нельзя!!!");
-		exit(0);
+		*errorCode = 1;
+		return 0;
 	}
 	if ((firstNumber >= 0 && secondNumber > 0) || (firstNumber <= 0 && secondNumber < 0)) {
 		if (abs(firstNumber) < abs(secondNumber)) {
-			exit(0);
+			*errorCode = 1;
 		}
 		else {
 			while (abs(firstNumber) >= abs(secondNumber)) {
-				firstNumber -= secondNumber;
-				result += 1;
+				firstNumber = firstNumber - secondNumber;
+				counter += 1;
 			}
 		}
 	}
 	else {
-		if (abs(firstNumber) < abs(secondNumber))
-			result = 0;
+		if (abs(firstNumber) < abs(secondNumber)) {
+			*errorCode = 1;
+		}
 		else {
 			while (abs(firstNumber) >= abs(secondNumber)) {
 				firstNumber += secondNumber;
-				result += 1;
+				counter += 1;
 			}
-			result *= (-1);
+			counter *= (-1);
 		}
 	}
-	printf("Неполное частное = %d\n", result);
-	return 0;
+	return counter;
+}
+
+int main(void) {
+	setlocale(LC_ALL, "Russian");
+	int errorCode = 0;
+	int result = incompleteQuotient(-432, 34, &errorCode);
+	if (errorCode == 1){
+		printf("Ошибка\n");
+	}
+	else {
+		printf("Неполное частное: %d\n", result);
+	}
 }
