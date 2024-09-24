b269b8a0298a0a408844f09fdb5a77ffcbfcd8ee
Andrw-404 <98185584+Andrw-404@users.noreply.github.com>
2024-09-24 21:41:58 +0300

    Update and rename homework4.c to Task4.c

--- Task4.c
+++ Task4.c
@@ -4,8 +4,8 @@ int main(void) {
 	int array[28] = { 0 };
 	for (int i = 0; i < 10; ++i) {
 		for (int k = 0; k < 10; ++k) {
-			for (int j = 0; k < 10; ++j) {
-				++array[i + k + j];
+			for (int j = 0; j < 10; ++j) {
+				++array[i + j + k];
 			}
 		}
 	}
