0bde9069e1c343dd9b630d018aa6fd3956b479c2
Andrw-404 <98185584+Andrw-404@users.noreply.github.com>
2024-09-25 12:02:55 +0300

    Update Task1.c

--- Task1.c
+++ Task1.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <locale.h>
 
-void counterOfZero(int array[]) {
-    setlocale(LC_ALL, "Russian");
-    int i = 0;
-    while (array[i] == 0) {
-        ++i;
+int counterOfZero(int array[], int size) {
+    int counter = 0;
+    for (int i = 0; i < size; i++) {
+        if (array[i] == 0) {
+            counter++;
+        }
     }
-    printf("Количество нулевых элементов в массиве: %d\nThe numbers of zero elements in the array: %d\n", i, i);
+    return counter;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int array[] = { 0,1,0,2 };
+    int size = sizeof(array) / sizeof(array[0]);
+    int c = counterOfZero(array, size);
+    printf("Количество нулевых элементов в массиве: %d\nThe numbers of zero elements in the array: %d\n", c, c);
+    return 0;
 }
